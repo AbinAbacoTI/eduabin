2ac6f29548d358a83f8951d97f6f608da03ce1e1
jhonmp1 <jhon.marca@tecsup.edu.pe>
2022-02-08 16:37:15 -0500

    navbar-actualizado 1.5

--- frontend-eduabin/src/components/common/Navbar/index.tsx
+++ frontend-eduabin/src/components/common/Navbar/index.tsx
@@ -21,7 +21,7 @@ function Navbar () {
 							<div className="hidden md:block">
 								<div className="ml-10 flex items-baseline space-x-4">
 
-								<div className=" bg-shadow">
+								<div className=" bg-white">
     								<div className="container  flex justify-center items-center px-4 sm:px-6 lg:px-8">
         								<div className="relative"> <input type="text" className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Busca el curso que quieras..."/>
           								  <div className="absolute top-4 right-3"> <i className="fa fa-search text-shadow-400 z-20 hover:text-shadow-500"></i> </div>
@@ -161,10 +161,23 @@ function Navbar () {
 						<div className="md:hidden" id="mobile-menu">
 							<div
 								ref={ref}
-								className="bg-blue px-2 pt-2 pb-3 space-y-1 sm:px-3"
+								className="bg-white px-2 pt-2 pb-3 space-y-1 sm:px-3"
 							>
+
+<div className="flex justify-center">
+  <div className="mb-3 xl:w-96">
+    <div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
+      <input type="search" className="form-control relative flex-auto min-w-0 block w-70 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none" placeholder="Busca el curso que desees..." aria-label="Search" aria-describedby="button-addon2"/>
+      <button className="btn inline-block px-3 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path fill="#ffffff " d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+</div>
 								<Link
-									href="/inicio"
+									href='/inicio'
 									to="inicio"
 									smooth="true"
 									offset={50}
@@ -174,7 +187,7 @@ function Navbar () {
 									Inicio
 								</Link>
 								<Link
-									href="/cursos"
+									href='/cursos'
 									to="cursos"
 									smooth="true"
 									offset={50}
@@ -185,7 +198,7 @@ function Navbar () {
 								</Link>
 
 								<Link
-									href="/eventos"
+									href='/eventos'
 									to="evento"
 									smooth="true"
 									offset={50}
@@ -195,7 +208,7 @@ function Navbar () {
 									Eventos
 								</Link>
 								<Link
-									href="/bussines"
+									href='/bussines'
 									to="bussines"
 									smooth="true"
 									offset={50}
@@ -205,38 +218,19 @@ function Navbar () {
 									Enseña en Adubin
 								</Link>
 
+								<div className="gap-4 grid-cols-2">
 								<Link
-									href="/auth/login"
+									href='/auth/login'
 									to="login"
 									smooth="true"
 									offset={50}
 									duration={500}
-									className="border-solid border-2 border-sky-50 cursor-pointer hover:bg-red-600 text-black hover:text-white block px-9 py-2 rounded-md text-base font-medium"
+									className="mt-10 mb-8 rounded-lg bg-orange-500 shadow-lg shadow-indigo-500/50 border-solid border-2 border-sky-50 cursor-pointer hover:bg-black text-white hover:text-white block px-9 py-2 rounded-md  text-xl  tracking-tight text-center font-bold"
 								>
-									Inicia Sesion
+									Inicia Sesion o Registrate
 								</Link>
 
-								<Link
-									href="/auth/register"
-									to="registro"
-									smooth="true"
-									offset={50}
-									duration={500}
-									className="border-solid border-2 border-sky-50 cursor-pointer hover:bg-red-600 text-black hover:text-white block px-9 py-2 rounded-md text-base font-medium"
-								>
-									Eres Nuevo, Registrate.
-								</Link>
-
-								<Link
-									href="/auth/register"
-									to="registro"
-									smooth="true"
-									offset={50}
-									duration={500}
-									className="border-solid border-2 border-sky-50 cursor-pointer hover:bg-red-600 text-black hover:text-white block px-9 py-2 rounded-md text-base font-medium"
-								>
-									Eres Nuevo, Registrate.
-								</Link>
+								</div>
 							</div>
 						</div>
 					)}
