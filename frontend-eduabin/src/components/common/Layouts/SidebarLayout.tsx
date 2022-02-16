bc0b42430eeb3bd449630503a5228b377afd8718
Joseph Antonio <joseph.carita@tecsup.edu.pe>
2022-02-16 10:19:22 -0500

    refactor(frontend-eduabin): code change in footer and sidebar

--- frontend-eduabin/src/components/common/Layouts/SidebarLayout.tsx
+++ frontend-eduabin/src/components/common/Layouts/SidebarLayout.tsx
@@ -2,11 +2,13 @@ import Sidebar from 'components/common/Sidebar'
 
 export default function SidebarLayout ({ children }) {
   return (
-    <>
-      <div>
+    <div className='flex'>
+      <div className='w-1/5 h-screen'>
         <Sidebar/>
       </div>
+      <div className='w-4/5 border-2 border-blue-500'>
       { children }
-    </>
+      </div>
+    </div>
   )
 }
--- frontend-eduabin/src/components/common/Sidebar/index.tsx
+++ frontend-eduabin/src/components/common/Sidebar/index.tsx
@@ -1,73 +1,39 @@
+import Puzzle from 'components/icons/Puzzle'
+import TablerGrid from 'components/icons/TablerGrid'
+import Link from 'components/ui/Link'
+
 export default function Sidebar (params) {
   return (
-        <div className="flex flex-no-wrap">
-            <div className="w-64 absolute sm:relative bg-gray-800 shadow md:h-full flex-col justify-between hidden sm:flex">
+        <div className="flex flex-no-wrap h-full">
+            <div className="w-full absolute sm:relative bg-gray-800 shadow md:h-full flex-col justify-between hidden sm:flex">
                 <div className="px-8">
                 <div className="image-text">
                 <span className="image">
                     <img src="https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/273035482_109862411613488_8272456011972948787_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeH2s2pZ1gCV7yLnI9fnhjnua_sYSqso_Cxr-xhKqyj8LIHrxm5tonhu97b_V61lKPwLHRcankrs3tWf0TrwL7MA&_nc_ohc=lxXAjVmBzDwAX_Avj45&_nc_ht=scontent.faqp1-1.fna&oh=00_AT9Qz1_g0TcZpyu0qB2AQENghTX8O6GbG4YrbHdatCTAKw&oe=621186B6" alt=""/>
                 </span>
-                <div className="text logo-text">
-                    <span className="name">EduAbin</span>
-                    <span className="profession">Abin</span>
+                <div className="text-center mt-3">
+                    <span className="text-white text-lg">Usuario</span>
                 </div>
             </div>
                     <ul className="mt-12">
-                        <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
+                        <Link href='/teacher'
+                        className="flex w-full justify-between text-gray-300 hover:text-gray-700 hover:bg-white cursor-pointer items-center mb-2 border- border-gray-300 py-2 px-2 rounded transition-all duration-200">
                             <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <rect x={4} y={4} width={6} height={6} rx={1} />
-                                    <rect x={14} y={4} width={6} height={6} rx={1} />
-                                    <rect x={4} y={14} width={6} height={6} rx={1} />
-                                    <rect x={14} y={14} width={6} height={6} rx={1} />
-                                </svg>
+                                <TablerGrid/>
                                 <span className="text-sm  ml-2">Dashboard</span>
                             </div>
-                            <div className="py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs">5</div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                        </Link>
+                        <Link
+                            href='/teacher/my-courses'
+                         className="flex w-full justify-between text-gray-300 hover:text-gray-700 hover:bg-white cursor-pointer items-center mb-2 border-0 border-gray-300 py-2 px-2 rounded transition-all duration-200">
                             <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                                </svg>
-                                <span className="text-sm  ml-2">Products</span>
+                                <Puzzle/>
+                                <span className="text-sm  ml-2">Mis Cursos</span>
                             </div>
-                            <div className="py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs">8</div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
-                            <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-compass" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <polyline points="8 16 10 10 16 8 14 14 8 16" />
-                                    <circle cx={12} cy={12} r={9} />
-                                </svg>
-                                <span className="text-sm  ml-2">Performance</span>
-                            </div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
-                            <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-code" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <polyline points="7 8 3 12 7 16" />
-                                    <polyline points="17 8 21 12 17 16" />
-                                    <line x1={14} y1={4} x2={10} y2={20} />
-                                </svg>
-                                <span className="text-sm  ml-2">Deliverables</span>
-                            </div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
-                            <div className="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                                </svg>
-                                <span className="text-sm  ml-2">Invoices</span>
-                            </div>
-                            <div className="py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs">25</div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                        </Link>
+                        <Link
+                            href='/teacher/course/create'
+                            className="flex w-full justify-between text-gray-300 hover:text-gray-700 hover:bg-white cursor-pointer items-center mb-2 border-0 border-gray-300 py-2 px-2 rounded transition-all duration-200">
                             <div className="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-stack" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" />
@@ -75,30 +41,27 @@ export default function Sidebar (params) {
                                     <polyline points="4 12 12 16 20 12" />
                                     <polyline points="4 16 12 20 20 16" />
                                 </svg>
-                                <span className="text-sm  ml-2">Courses</span>
+                                <span className="text-sm  ml-2">Crear curso</span>
                             </div>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center">
+                        </Link>
+                        <Link
+                            href='/teacher/notifications'
+                            className="flex w-full justify-between text-gray-300 hover:text-gray-700 hover:bg-white cursor-pointer items-center mb-2 border-0 border-gray-300 py-2 px-2 rounded transition-all duration-200">
                             <div className="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-settings" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" />
                                     <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                                     <circle cx={12} cy={12} r={3} />
                                 </svg>
-                                <span className="text-sm  ml-2">Settings</span>
+                                <span className="text-sm  ml-2">Notificaciones</span>
                             </div>
-                        </li>
+                        </Link>
                     </ul>
                     <div className="flex justify-center mt-48 mb-4 w-full">
-                        <div className="relative ">
-                            <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <circle cx={10} cy={10} r={7} />
-                                    <line x1={21} y1={21} x2={15} y2={15} />
-                                </svg>
-                            </div>
-                            <input className=" bg-gray-700 focus:outline-none rounded w-full text-sm text-gray-500 bg-gray-100 pl-10 py-2" type="text" placeholder="Search" />
+                        <div className="relative">
+                            <button
+                                className='text-white py-1 px-4 bg-red-600 rounded-md'
+                            >Cerrar sesion</button>
                         </div>
                     </div>
                 </div>
@@ -137,7 +100,9 @@ export default function Sidebar (params) {
                 </div>
             </div>
             <div className="w-64 z-40 absolute bg-gray-800 shadow md:h-full flex-col justify-between sm:hidden  transition duration-150 ease-in-out" id="mobile-nav">
-                <div className="h-10 w-10 bg-gray-800 absolute right-0 mt-16 -mr-10 flex items-center shadow rounded-tr rounded-br justify-center cursor-pointer" id="mobile-toggler" onClick="sidebarHandler()">
+                <div className="h-10 w-10 bg-gray-800 absolute right-0 mt-16 -mr-10 flex items-center shadow rounded-tr rounded-br justify-center cursor-pointer"
+                id="mobile-toggler">
+
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-adjustments" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" />
                         <circle cx={6} cy={10} r={2} />
@@ -283,9 +248,6 @@ export default function Sidebar (params) {
                         </li>
                     </ul>
                 </div>
-            </div>
-            <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-                <div className="w-full h-full rounded border-dashed border-2 border-gray-300"></div>
             </div>
         </div>
   )
