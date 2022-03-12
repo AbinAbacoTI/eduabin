f5ca946bc955a07d9cfdbb318bffbbbc877abf7c
Joseph Antonio <joseph.carita@tecsup.edu.pe>
2022-03-12 10:10:28 -0500

    refactor(frontend-eduabin): change of code and styles in cards

--- frontend-eduabin/tailwind.config.js
+++ frontend-eduabin/tailwind.config.js
@@ -6,14 +6,19 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        600: '600px'
+        600: '600px',
+        650: '650px',
+        700: '700px',
+        400: '400px'
       },
       width: {
-        306: '306px'
+        306: '306px',
+        73: '73%'
       },
       colors: {
         'orange-eduabin': '#E9581B',
-        'white-ext': '#F2F4F6'
+        'white-ext': '#F2F4F6',
+        'blue-transparent': 'rgba(21, 71, 210, 0.67)'
       }
     }
   },
