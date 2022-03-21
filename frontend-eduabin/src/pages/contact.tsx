e2ebf98619915b4ae518a83cefff90a99fa2ca27
Joseph Antonio <joseph.carita@tecsup.edu.pe>
2022-03-21 16:36:51 -0500

    refactor(frontend-eduabin): code cleanup and modification

--- frontend-eduabin/src/pages/contact.tsx
+++ frontend-eduabin/src/pages/contact.tsx
@@ -1,20 +1,10 @@
 import Layout from 'components/common/Layouts/Layout'
 import Image from 'next/image'
-import { motion as m } from 'framer-motion'
-import Slider from 'components/ui/Slider'
-import CardCourse from '../components/Cards/CardCourse/index'
 
 const Contact = () => {
-  const images = [
-    '/images/image1.jpeg',
-    '/images/image2.jpeg',
-    '/images/image3.jpeg',
-    '/images/image4.jpeg'
-  ]
-
   return (
     <Layout>
-      {/* <section className='w-full h-400 relative'>
+      <section className='w-full h-400 relative'>
         <Image src={'/images/bg202203.jpg'}
           layout='fill'
           className='object-bottom object-cover pointer-events-none brightness-50'
@@ -25,20 +15,7 @@ const Contact = () => {
             <span>Home / Contacto</span>
           </div>
         </div>
-      </section> */}
-      <div className='py-20'>
-        <Slider>
-          <div className='p-10'>
-          <CardCourse/>
-          </div>
-          <div className='p-10'>
-          <CardCourse/>
-          </div>
-          <div className='p-10'>
-          <CardCourse/>
-          </div>
-        </Slider>
-      </div>
+      </section>
     </Layout>
   )
 }
